e0c78812e0f25eccfa9351e012724856878f19d9
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-07-08 23:03:25 +0300

    Created `task3`

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/screens/login_screen.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
+        width: size.width,
+        height: size.height,
         decoration: const BoxDecoration(
           color: Colors.deepPurple,
           image: DecorationImage(
@@ -16,158 +20,56 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 50),
-            Image.asset(
-              'assets/images/logo.png',
-              height: MediaQuery.of(context).size.height * 0.1,
-              width: MediaQuery.of(context).size.width * 0.3,
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                height: size.height * 0.1,
+                width: size.width * 0.3,
+              ),
             ),
-            const SizedBox(height: 10),
-            Image.asset(
-              'assets/images/info.png',
-              height: MediaQuery.of(context).size.height * 0.2,
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  ),
-                  color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'ITI Quiz App',
+                style: GoogleFonts.roboto(
+                  textStyle: const TextStyle(fontSize: 22, color: Colors.white),
                 ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 35),
-                    Text(
-                      'Login',
-                      style: GoogleFonts.roboto(
-                        textStyle: const TextStyle(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: .5),
-                      ),
+              ),
+            ),
+            Text(
+              'We enjoy spreading knowledge',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            const Spacer(),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 75, vertical: 30),
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyLoginPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.roboto(
+                    textStyle: const TextStyle(
+                      fontSize: 22,
                     ),
-                    const SizedBox(height: 25),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Username',
-                        hintStyle: GoogleFonts.roboto(
-                          textStyle: const TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        prefixIcon: const Icon(Icons.person),
-                        contentPadding: const EdgeInsets.all(15),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 18),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: GoogleFonts.roboto(
-                          textStyle: const TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        prefixIcon: const Icon(Icons.lock),
-                        contentPadding: const EdgeInsets.all(15),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'New To Quizizz?',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        GestureDetector(
-                          child: TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                            child: const Text('Register?'),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 75, vertical: 15),
-                      width: double.infinity,
-                      height: 45,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: Text(
-                          'Login',
-                          style: GoogleFonts.roboto(
-                            textStyle: const TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 70, vertical: 15),
-                      child: Image.asset(
-                        'assets/images/print.png',
-                        height: MediaQuery.of(context).size.height * 0.07,
-                        width: MediaQuery.of(context).size.width * 0.2,
-                      ),
-                    ),
-                    const Text(
-                      'Use Touch ID',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        Checkbox(
-                          value: false,
-                          onChanged: (value) {},
-                        ),
-                        const Text(
-                          'Remember me',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        const Spacer(),
-                        GestureDetector(
-                          child: TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                            child: const Text(
-                              'Forgot Password?',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyLoginPage extends StatelessWidget {
+  const MyLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            Image.asset(
+              'assets/images/logo.png',
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.3,
+            ),
+            const SizedBox(height: 10),
+            Image.asset(
+              'assets/images/info.png',
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 35),
+                    Text(
+                      'Login',
+                      style: GoogleFonts.roboto(
+                        textStyle: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: .5),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: GoogleFonts.roboto(
+                          textStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        prefixIcon: const Icon(Icons.person),
+                        contentPadding: const EdgeInsets.all(15),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 18),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: GoogleFonts.roboto(
+                          textStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        prefixIcon: const Icon(Icons.lock),
+                        contentPadding: const EdgeInsets.all(15),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'New To Quizizz?',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        GestureDetector(
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: const Text('Register?'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 75, vertical: 15),
+                      width: double.infinity,
+                      height: 45,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: Text(
+                          'Login',
+                          style: GoogleFonts.roboto(
+                            textStyle: const TextStyle(
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 70, vertical: 15),
+                        child: const Icon(
+                          Icons.fingerprint,
+                          size: 65,
+                          color: Colors.blue,
+                        )),
+                    const Text(
+                      'Use Touch ID',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (value) {},
+                        ),
+                        const Text(
+                          'Remember me',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const Spacer(),
+                        GestureDetector(
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
